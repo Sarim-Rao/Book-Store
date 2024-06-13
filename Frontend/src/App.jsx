465296d333276fa64ac2c5b47be7364493639a01
sarim <sarimrana819@gmail.com>
2024-06-13 17:41:24 +0500

    creating navbar not complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./Components/Navbar/index"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Constants/Constants.js
@@ -0,0 +1,22 @@
+export const NavItems = [
+  {
+    id: 1,
+    Title: "Home",
+    Link: "/home",
+  },
+  {
+    id: 1,
+    Title: "Course",
+    Link: "/home",
+  },
+  {
+    id: 1,
+    Title: "Contact",
+    Link: "/home",
+  },
+  {
+    id: 1,
+    Title: "About",
+    Link: "/home",
+  },
+];
